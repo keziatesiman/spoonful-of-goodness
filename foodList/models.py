f807eb008dc90daa03d276019bd63f3f30c60679
Kezia Irene <kezia.irene@ui.ac.id>
2019-04-14 19:27:55 +0700

    add models for recipes

--- foodList/models.py
+++ foodList/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    recipeName = models.CharField(max_length=50)
+    recipeCalories = models.CharField(max_length=50)
+    ingredients = models.CharField(max_length=100)
+    time = models.CharField(max_length=50)
+    serving = models.CharField(max_length=50)
+
+    pork = models.BooleanField(default=False)
+    alcohol = models.BooleanField(default=False)
+    gluten = models.BooleanField(default=False)
+    lactose = models.BooleanField(default=False)
+    egg = models.BooleanField(default=False)
+    meat = models.BooleanField(default=False)
+    vegan = models.BooleanField(default=False)
+    containsMilk = models.BooleanField(default=False)
+    containsMilkSubstitute = models.BooleanField(default=False)
