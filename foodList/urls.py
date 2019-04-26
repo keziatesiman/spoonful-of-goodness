7ea914c055d36fc214a770606b3566dbb242695e
Kezia Irene <kezia.irene@ui.ac.id>
2019-04-26 13:56:41 +0700

    make food list menu and edit header plus statics

--- foodList/urls.py
+++ foodList/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name='foodList'
 urlpatterns = [
+    path('', views.index, name="index"),
     path('breadFruitPie/', views.breadFruitPie, name="breadFruitPie"),
 ]
--- foodList/views.py
+++ foodList/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+def index(request):
+    return render(request, 'foodList/templates/index.html', {})
+
 def breadFruitPie(request):
     return render(request, 'foodList/templates/breadFruitPie.html', {})
