b6ef04043cd93ec0b13b8f3c2dc65b0f68b89784
Irwanto <irwanto_kong@yahoo.co.id>
2019-05-03 13:54:22 +0700

    Changed method to filter domain

--- foodRecommendation/views.py
+++ foodRecommendation/views.py
@@ -4,6 +4,8 @@ from foodList.models import Recipe
 
 import json
 
+assigned = []
+
 def food_recommendation(request):
     # filter domain by ingredients constraints
     domain = filter_domain(request)
@@ -15,33 +17,81 @@ def food_recommendation(request):
     # get number of meals per day
     meals_per_day = request.GET.get('meals_per_day')
 
+    # assign_assigned(len(domain))
+    # search_food(domain, total_calories, meals_per_day)
+
     return JsonResponse(domain, safe=False)
 
 # method to filter domain by ingredients constraints
 def filter_domain(request):
+    domain = Recipe.objects.all()
+    
     alcohol = request.GET.get('contains_alcohol')
-    gluten = request.GET.get('contains_gluten')
-    lactose = request.GET.get('contains_lactose')
-    egg = request.GET.get('contains_egg')
-    meat = request.GET.get('contains_meat')
-    pork = request.GET.get('contains_pork')
-    fish = request.GET.get('contains_fish')
-    vegan = request.GET.get('is_vegan')
-    containsMilk = request.GET.get('contains_milk')
-    containsMilkSubstitute = request.GET.get('contains_milk_substitute')
+    if (alcohol == "False"):
+        domain = domain.filter(alcohol=False)
 
-    domain = Recipe.objects.filter(pork=pork, alcohol=alcohol, gluten=gluten, lactose=lactose, egg=egg, #fish=fish,
-                vegan=vegan, meat=meat, containsMilk=containsMilk, containsMilkSubstitute=containsMilkSubstitute).values()
-    return list(domain)
+    gluten = request.GET.get('contains_gluten')
+    if (gluten == "False"):
+        domain = domain.filter(gluten=False)
+
+    lactose = request.GET.get('contains_lactose')
+    if (lactose == "False"):
+        domain = domain.filter(lactose=False)
+
+    egg = request.GET.get('contains_egg')
+    if (egg == "False"):
+        domain = domain.filter(egg=False)
+    
+    meat = request.GET.get('contains_meat')
+    if (meat == "False"):
+        domain = domain.filter(meat=False)
+
+    pork = request.GET.get('contains_pork')
+    if (pork == "False"):
+        domain = domain.filter(pork=False)
+    
+    fish = request.GET.get('contains_fish')
+    if (fish == "False"):
+        domain = domain.filter(fish=False)
+
+    vegan = request.GET.get('is_vegan')
+    if (vegan == "True"):
+        domain = domain.filter(vegan=True)
+    
+    containsMilk = request.GET.get('contains_milk')
+    if (containsMilk == "False"):
+        domain = domain.filter(containsMilk=False)
+
+    containsMilkSubstitute = request.GET.get('contains_milk_substitute')
+    if (containsMilkSubstitute == "False"):
+        domain = domain.filter(containsMilkSubstitute=False)
+
+    print(domain)
+    return list(domain.values())
 
 # method to get number of calories by calory type
 def get_calories(calory_type):
     if (calory_type == 'Diet'):
-        return 1000
+        return 1200
     if (calory_type == 'Normal'):
         return 2000
     if (calory_type == 'High Calories'):
         return 3000
 
-def search_food():
-    pass
+def assign_assigned(n):
+    for i in range(n+1):
+        assigned[i] = False
+
+def search_food(domain, total_calories, meals_per_day):
+    if meals_per_day == 0:
+        if abs(total_calories) < 100:
+            return True
+
+    for food in domain:
+        if assigned[food['id']]:
+            pass
+        else:
+            assigned[food['id']] = True
+            if search_food(domain, total_calories - food['recipeCalories'], meals_per_day - 1):
+                return
+        assigned[food['id']] = False
