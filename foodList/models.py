f110bf1a3ba9bb31f92af5023f2647c7912d2db8
Kezia Irene <kezia.irene@ui.ac.id>
2019-05-03 20:36:31 +0700

    finish food list

--- foodList/models.py
+++ foodList/models.py
@@ -3,9 +3,6 @@ from django.db import models
 class Recipe(models.Model):
     recipeName = models.CharField(max_length=50)
     recipeCalories = models.CharField(max_length=50)
-    ingredients = models.CharField(max_length=100)
-    time = models.CharField(max_length=50)
-    serving = models.CharField(max_length=50)
 
     pork = models.BooleanField(default=False)
     alcohol = models.BooleanField(default=False)
@@ -14,5 +11,6 @@ class Recipe(models.Model):
     egg = models.BooleanField(default=False)
     meat = models.BooleanField(default=False)
     vegan = models.BooleanField(default=False)
+    fish = models.BooleanField(default=False)
     containsMilk = models.BooleanField(default=False)
     containsMilkSubstitute = models.BooleanField(default=False)
--- foodList/views.py
+++ foodList/views.py
@@ -5,3 +5,42 @@ def index(request):
 
 def breadFruitPie(request):
     return render(request, 'foodList/templates/breadFruitPie.html', {})
+
+def basilPestoQuinoa(request):
+    return render(request, 'foodList/templates/basilPestoQuinoa.html', {})
+
+def beefCutlet(request):
+    return render(request, 'foodList/templates/beefCutlet.html', {})
+
+def carrotSticksWithDip(request):
+    return render(request, 'foodList/templates/carrotSticksWithDip.html', {})
+
+def chocolateMilkShake(request):
+    return render(request, 'foodList/templates/chocolateMilkShake.html', {})
+
+def doubleVeganPizzaWithSoyMilk(request):
+    return render(request, 'foodList/templates/doubleVeganPizzaWithSoyMilk.html', {})
+
+def lentilNutTacos(request):
+    return render(request, 'foodList/templates/lentilNutTacos.html', {})
+
+def mushroomFriedRice(request):
+    return render(request, 'foodList/templates/mushroomFriedRice.html', {})
+
+def peanutRaisinOatmeal(request):
+    return render(request, 'foodList/templates/peanutRaisinOatmeal.html', {})
+
+def penneArabiataWithSoyMilk(request):
+    return render(request, 'foodList/templates/penneArabiataWithSoyMilk.html', {})
+
+def pumpkinPie(request):
+    return render(request, 'foodList/templates/pumpkinPie.html', {})
+
+def soyMilk(request):
+    return render(request, 'foodList/templates/soyMilk.html', {})
+
+def veganLasagnaRolls(request):
+    return render(request, 'foodList/templates/veganLasagnaRolls.html', {})
+
+def veganPizzaWithSoyMilk(request):
+    return render(request, 'foodList/templates/veganPizzaWithSoyMilk.html', {})
